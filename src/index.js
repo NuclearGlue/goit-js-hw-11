1bf26e7be4f8e71ca77a92a63c673bbd783c860e
Alexandr Sorokin <sanek1678@gmail.com>
2021-12-14 22:04:20 +0200

    small

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+import API from '/request-image';
 
-import getImage from '/request-image';
-const search = 'Puppy';
+const seekForm =document.querySelector('.search-form')
+const searchValue = document.querySelector('input[name="searchQuery"]');
+const seekImg = searchValue.textContent;
+
+seekForm.addEventListener("submit", imageSearch);
+
+function imageSearch(event) { 
+    event.preventDefault();
+    console.log(seekImg);
+    for (let i = 1; i > 0; i += 1){
+        API.getImage(seekImg,i);
+    }
+     
+}
 
 
-
-getImage(search);
--- src/request-image.js
+++ src/request-image.js
@@ -1,14 +1,19 @@
 const axios = require('axios');
-function getImage(search) {
-     axios.get('https://pixabay.com/api/', {
-        params: {
+
+const getImage = async (search, page) =>{
+     const imageList = await axios.get('https://pixabay.com/api/', {
+       params: {
+         page: page,
+         per_page: 40,
             key: '24778461-5d71b0865fcbfdadabb130a0c',
-            q: search,
+         q: `${ search }`,
             image_type: 'photo',
             orientation: 'horizontal',
             safesearch: true,
-
-  }});
+  }})
+    ;
+  console.log(search);
+  return imageList;
 }
 
 export default { getImage };
