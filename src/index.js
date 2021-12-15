3048250274b5784e43d0d654b304a608c22cdf53
Alexandr Sorokin <sanek1678@gmail.com>
2021-12-15 17:25:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import API from '/request-image';
 
 const seekForm =document.querySelector('.search-form')
 const searchValue = document.querySelector('input[name="searchQuery"]');
-const seekImg = searchValue.textContent;
+
+const loadMore = document.querySelector('.load-more');
 
 seekForm.addEventListener("submit", imageSearch);
 
 function imageSearch(event) { 
     event.preventDefault();
-    console.log(seekImg);
-    for (let i = 1; i > 0; i += 1){
-        API.getImage(seekImg,i);
-    }
+    const seekImg = searchValue.value;
+    let pageNumber = 1;
+
+    API.getImage(seekImg, pageNumber);
+    
+    loadMore.addEventListener('click', () => {
+        pageNumber += 1;
+         console.log(pageNumber);
+        API.getImage(seekImg, pageNumber);  
+    })
+        
+    
      
 }
 
